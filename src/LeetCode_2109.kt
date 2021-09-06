900a89edaa05a93755575e9933101db36812cda3
coegle18 <coegle18@gmail.com>
2021-09-06 19:19:09 +0800

    添加 704. 二分查找

--- src/LeetCode_2109.kt
+++ src/LeetCode_2109.kt
@@ -30,13 +30,34 @@ fun smallestK(arr: IntArray, k: Int): IntArray {
     }
     return ans.toIntArray()
 }
+
+// 704. 二分查找
+fun search(nums: IntArray, target: Int): Int {
+    var left = 0
+    var right = nums.size - 1
+    while (left <= right) {
+        val mid = left + (right - left) / 2
+        when {
+            nums[mid] == target -> {
+                return mid
+            }
+            nums[mid] > target -> {
+                right = mid - 1
+            }
+            else -> {
+                left = mid + 1
+            }
+        }
+    }
+    return -1
+}
 fun main () {
     val array = intArrayOf(3)
-    val intArray1 = intArrayOf(2,3,7,6)
+    val intArray1 = intArrayOf(-1,0,3,5,9,12)
     val intList1 = mutableListOf(1, 2, 3)
     val intList2 = mutableListOf(1, 2, 3)
     val str = "cbbd"
     val arrayIntArray = arrayOf(intArrayOf(1), intArrayOf(0,2,4), intArrayOf(1,3,4), intArrayOf(2), intArrayOf(1,2))
-    val ans = smallestK(intArray1, 2)
-    println(ans.contentToString())
+    val ans = search(intArray1, 2)
+    println(ans)
 }
