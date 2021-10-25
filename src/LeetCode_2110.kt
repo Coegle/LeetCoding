2dfce3d5fc0ee0473cb0b70a387c862edf69df63
coegle18 <coegle18@gmail.com>
2021-10-25 16:11:33 +0800

    添加 240. 搜索二维矩阵 II

--- src/LeetCode_2110.kt
+++ src/LeetCode_2110.kt
@@ -10,15 +10,32 @@ fun constructRectangle(area: Int): IntArray {
     return intArrayOf(area, 1)
 }
 
+// 240. 搜索二维矩阵 II
+fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
+    var x = 0
+    var y = matrix[0].size - 1
+    while (x >= 0 && y < matrix.size) {
+        val thisVal = matrix[x][y]
+        if (thisVal == target) return true
+        else if (thisVal > target) {
+            y--
+        }
+        else {
+            x++
+        }
+    }
+    return false
+}
+
 fun main () {
     val array = intArrayOf(1,2,3)
     val intArray1 = intArrayOf(9,1)
     val intList1 = mutableListOf(1, 2, 3)
     val intList2 = mutableListOf(1, 2, 3)
     val str = "cbbd"
-    val arrayIntArray = arrayOf(intArrayOf(1), intArrayOf(0,2,4), intArrayOf(1,3,4), intArrayOf(2), intArrayOf(1,2))
+    val arrayIntArray = arrayOf(intArrayOf(1,2,3,4,5), intArrayOf(6,7,8,9,10), intArrayOf(11,12,13,14,15), intArrayOf(16,17,18,19,20), intArrayOf(21,22,23,24,25))
     val arrayOfStrings = arrayOf("Science","is","what","we","understand","well","enough","to","explain",
         "to","a","computer.","Art","is","everything","else","we","do")
-    val ans = constructRectangle(17)
-    println(ans.contentToString())
+    val ans = searchMatrix(arrayIntArray, 20)
+    println(ans)
 }
